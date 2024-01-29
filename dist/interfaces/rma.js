0b59bd2170a90cce971a00f6fde99d9f2a656e2b
Harry@mac <Harry@mac>
2024-01-29 14:30:55 +1100

    remove dist

--- dist/interfaces/rma.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.EMPTY_RMA_INTERNAL_NOTE = void 0;
-exports.EMPTY_RMA_INTERNAL_NOTE = {
-    note: "",
-    images: [],
-    createdAt: 0,
-    createdBy: "",
-};
